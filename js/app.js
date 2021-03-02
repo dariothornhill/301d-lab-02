44d4dfdb60b349a3606afa744793b15b2e6c5e8f
Dario Thornhill <dario.thornhill@gmail.com>
2021-03-02 01:07:22 +0100

    Styled UI and Added sort

--- js/app.js
+++ js/app.js
@@ -8,34 +8,101 @@ function Animal({image_url,title, description, keyword, horns }) {
   this.horns = horns;
 }
 
-function displayImages(data) {
+function clear() {
+  $('main').empty();
+}
+
+function displayImages(data, cols = 4) {
   const template = $('#photo-template');
-  data.forEach(item => {
-    const element = $(`<div data-keyword='${item.keyword}'></div>`);
+  let row = $('<div class="row"></div>');
+  data.forEach((item, index) => {
+    const element = $(`<div class="col-1-4 left card" data-keyword='${item.keyword}'></div>`);
     element.html($(template.html()));
+
     $(element).find('h2').text(item.title);
     $(element).find('img').attr({ src: item.src, alt: item.description});
-    $(element).find('p').text('Horns: ' + item.horns);
-    $('main').append(element);
+    $(element).find('span').text('Horns: ' + item.horns);
+    $(element).find('p').text(item.description);
+
+    $(row).append(element);
+    if (index % cols === cols - 1 || index === data.length-1) {
+      $('main').append(row);
+      row = $('<div class="row"></div>');
+    }
   });
 }
 
 function populateOptions(data) {
   const options = [...new Set(data.map(item => item.keyword))];
+  const optionElements = options.map(item => $(`<option value='${item}'>${item}</option>`));
+
   const selectControl = $('#filterSelector');
-  const optionElements = options.map(o => $(`<option value='${o}'>${o}</option>`));
   selectControl.append(optionElements);
   selectControl.on('change', (event) => {
     const keyword = event.currentTarget.value;
-    $('main div').hide();
+    $('main div.row div').hide();
     $(`[data-keyword='${keyword}']`).show();
+    const filtered = (keyword === 'default') ? data : data.filter(d => (d.keyword === keyword));
+    clear();
+    console.log(filtered);
+    displayImages(filtered, 4);
   });
 }
 
-$(function () {
-  $.ajax('/data/page-1.json').then(data => {
-    const animals = data.map(item => new Animal(item));
-    displayImages(animals);
-    populateOptions(animals);
+function sortByName(animals) {
+  return animals.sort((a, b) => {
+    if(a.title > b.title) {
+      return 1;
+    }
+
+    if (a.title === b.title) {
+      return 0;
+    }
+
+    if (a.title < b.title) {
+      return -1;
+    }
   });
+}
+
+function sortByHorns(animals) {
+  return animals.sort((a, b) => {
+    if(a.horns > b.horns) {
+      return 1;
+    }
+
+    if (a.horns === b.horns) {
+      return 0;
+    }
+
+    if (a.horns < b.horns) {
+      return -1;
+    }
+  });
+}
+
+
+$(function () {
+  const state = {};
+
+  $.ajax('/data/page-1.json').then(data => {
+    state.animals = data.map(item => new Animal(item));
+    const sorted = sortByName(state.animals);
+    console.log(sorted);
+    displayImages(sorted, 4);
+    populateOptions(sorted);
+  });
+
+  $('#btnNameSort').on('click', () => {
+    const sorted = sortByName(state.animals);
+    clear();
+    displayImages(sorted, 4);
+  });
+
+  $('#btnHornSort').on('click', () => {
+    const sorted = sortByHorns(state.animals);
+    clear();
+    displayImages(sorted, 4);
+  });
+
 });
--- user_stories.md
+++ user_stories.md
@@ -2,7 +2,6 @@
 
 - [x] As a user, I want to view the image on the page so that I can browse the photo collection.
 - [x] As a user, I want to be able to filter the images so that I can view only images that match a keyword.
-- [ ] As a user, I want a simple, clean looking UI so that my photo gallery clearly displays the images in a grid like pattern.
-- [ ] As a user, I want to be able to sort the images so there is an order to how they render.
+- [x] As a user, I want a simple, clean looking UI so that my photo gallery clearly displays the images in a grid like pattern.
 ## Stretch
-- [ ] As a user, I want to be able to sort the images so there is an order to how they render.
+- [x] As a user, I want to be able to sort the images so there is an order to how they render.
